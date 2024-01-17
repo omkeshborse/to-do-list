f4c7137201e6f2df56ad78f766b498729ebefd54
omkeshborse <bomkesh25@gmail.com>
2024-01-17 17:08:34 +0530

    add task functionality

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
- 
+const addForm = document.querySelector(".add");
+const tasks = document.querySelector(".task");
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = addForm.task.value.trim();
+  if (value.length) {
+    tasks.innerHTML += `<li><span>${value}</span>
+                            <i class="bi bi-trash delete"></i>
+                        </li>`;
+    addForm.reset();
+  }
+});

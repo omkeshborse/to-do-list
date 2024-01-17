c09bb322a61b861920969833ec8a30fa63d6a052
omkeshborse <bomkesh25@gmail.com>
2024-01-17 17:48:30 +0530

    updated list count in todo list

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const addForm = document.querySelector(".add");
 const tasks = document.querySelector(".task");
 const clearAll = document.querySelector(".clear");
+const messageSpan = document.querySelector(".message span");
+
+function updatedMessage() {
+  const tasksLength = tasks.children.length;
+  messageSpan.textContent = `You have ${tasksLength} task pending`;
+}
+updatedMessage();
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const value = addForm.task.value.trim();
@@ -9,18 +16,21 @@ addForm.addEventListener("submit", (e) => {
                             <i class="bi bi-trash delete"></i>
                         </li>`;
     addForm.reset();
+    updatedMessage();
   }
 });
 
 tasks.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
+    updatedMessage();
   }
 });
 
-clearAll.addEventListener("click" , (e)=>{
-    const taskItems = tasks.querySelectorAll("li") ;
-    taskItems.forEach((task)=>{
-        task.remove();
-    })
-})
+clearAll.addEventListener("click", (e) => {
+  const taskItems = tasks.querySelectorAll("li");
+  taskItems.forEach((task) => {
+    task.remove();
+    updatedMessage();
+  });
+});
